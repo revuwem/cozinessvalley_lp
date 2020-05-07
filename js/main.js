e5e080bd678993d971ae82271a144ea6026b4e45
revuwem <revuwem@gmail.com>
2020-05-07 22:14:20 +0400

    created preventing of bg fixed bug on ios with bg-not-fixed and ios-bg classes

--- js/main.js
+++ js/main.js
@@ -16,9 +16,24 @@ $(".nav-items .nav-link").on("click", function () {
 });
 
 
-//Safari browser detection
+//Safari browser detection for handle background:fixed bug
+const valley_overview=document.getElementById("valley-overview");
+const situation_plan=document.getElementById("situation-plan");
 if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) 
-{
-  const valley_overview=document.getElementById("valley-overview");
-  valley_overview.classList.add("bg-not-fixed");
-}
+{ 
+    if(!valley_overview.classList.contains("bg-not-fixed")){
+        valley_overview.classList.add("bg-not-fixed");
+    }  
+
+    if(!situation_plan.classList.contains("ios-bg")){
+        situation_plan.classList.add("ios-bg");
+    }
+}else{
+    if(valley_overview.classList.contains("bg-not-fixed")){
+        valley_overview.classList.remove("bg-not-fixed");
+    }
+
+    if(situation_plan.classList.contains("ios-bg")){
+        situation_plan.classList.remove("ios-bg");
+    }
+};
